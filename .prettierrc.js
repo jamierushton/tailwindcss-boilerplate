16f3883df15c7f40091a440fc2fa36ac6e83ece2
Jamie Rushton <jamie.rushton@perspicuity.co.uk>
2019-08-31 21:22:40 +0100

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+};
